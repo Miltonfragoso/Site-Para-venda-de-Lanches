f43c801c1d53ec27e6c8094cbd359d5f0a4d9f66
Milton Fragoso <fragosomilton54@gmail.com>
2022-06-23 17:58:38 -0300

    Iniciando a implementação do Controller e Views

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        //injeção de dependencia do REPOSITORY
+        private LancheRepository _lancheRepository;
+        private CategoriaRepository _categoriaRepository;
+
+        public LancheController(LancheRepository lancheRepository,
+                                CategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+
+        }
+    }
+}
